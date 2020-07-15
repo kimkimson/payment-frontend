b24a6df3cde3ce35626bcb94ed809ec655bd9900
Nguyen Kim Son <ohflashback@gmail.com>
2020-07-15 16:56:08 +0700

    Updates

---
+++ precache-manifest.cf38dfa071811d0b4ed1b2e2e06a7878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933da5ac0a21f4e49854542ec6db7c93",
+    "url": "/kimkimson.github.io/frontend-payment/index.html"
+  },
+  {
+    "revision": "ef01510fbeb6f5141181",
+    "url": "/kimkimson.github.io/frontend-payment/static/css/main.eb46cc77.chunk.css"
+  },
+  {
+    "revision": "3bc4bbfba81b21d5ef12",
+    "url": "/kimkimson.github.io/frontend-payment/static/js/2.ebec0fd0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kimkimson.github.io/frontend-payment/static/js/2.ebec0fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef01510fbeb6f5141181",
+    "url": "/kimkimson.github.io/frontend-payment/static/js/main.2ccf7655.chunk.js"
+  },
+  {
+    "revision": "1aeb67e4f5222ac5a655",
+    "url": "/kimkimson.github.io/frontend-payment/static/js/runtime-main.0ed5cf64.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d0a48d074b184ce2649050bb470ed8e.js"
+  "/kimkimson.github.io/frontend-payment/precache-manifest.cf38dfa071811d0b4ed1b2e2e06a7878.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kimkimson.github.io/frontend-payment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
